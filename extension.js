c4ebaecdbba5b65f988f8a3245f770abf1e9d27d
Ysky114 <1302524709@qq.com>
2026-05-08 16:54:27 +0800

    0.90.1(补丁)

--- extension.js
+++ extension.js
@@ -589,9 +589,6 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 				},
 				{
 					type: "players", data: [
-						"Rafau_yzs",
-						"Patchouli_Knowledge_yzs",
-						"Byakuren_yzs"
 					]
 				},
 			];
@@ -682,12 +679,12 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
             <div style="color:#ffa348">• 有问题可加群：</div><br>
             <div style="color:#ffa348">&nbsp;&nbsp;Q:1015772605</div><br>
             <div style="color:#ffa348">• 角色设计：御.sky/先天虚体阿阳/加农/海马吉人/Etermpty</div><br>
-            <div style="color:#ffa348">• 版本号：v0.90</div><br>
+            <div style="color:#ffa348">• 版本号：v0.90.1</div><br>
             `,
 			author: "御.sky",
 			diskURL: "",
 			forumURL: "",
-			version: "0.90",
+			version: "0.90.1",
 		},
 		files: {}, connect: true
 	}
